ba5a8c7f3c622e60b46ecee31869c8caaa505e38
RakeshThapa332 <thapaarakesh1@gmail.com>
2026-08-09 12:07:00 +0545

    Initial Data Inspection

--- backend/app/models/food.py
+++ backend/app/models/food.py
@@ -5,18 +5,33 @@ class Food(db.Model):
     __tablename__ = "foods"
 
     id = db.Column(db.Integer, primary_key=True)
+
+    # Food information
     food_name = db.Column(db.String(150), nullable=False)
-    food_name_nepali = db.Column(db.String(150))
-    category = db.Column(db.String(50))
-    calories = db.Column(db.Float)
+    edible_portion = db.Column(db.Float)
+
+    # Macronutrients / composition
+    moisture = db.Column(db.Float)
     protein = db.Column(db.Float)
     fat = db.Column(db.Float)
-    carbs = db.Column(db.Float)
+    carbohydrate = db.Column(db.Float)
+    minerals = db.Column(db.Float)
     fiber = db.Column(db.Float)
+
+    # Energy
+    energy = db.Column(db.Float)
+
+    # Minerals
     calcium = db.Column(db.Float)
+    phosphorus = db.Column(db.Float)
     iron = db.Column(db.Float)
-    vitamin_a = db.Column(db.Float)
+
+    # Vitamins
+    carotene = db.Column(db.Float)
     vitamin_c = db.Column(db.Float)
-    sodium = db.Column(db.Float)
-    potassium = db.Column(db.Float)
+    thiamine = db.Column(db.Float)
+    riboflavin = db.Column(db.Float)
+    niacin = db.Column(db.Float)
+
+    # Generated later by ML
     cluster_id = db.Column(db.Integer)
