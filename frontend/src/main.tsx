1647951e9bc1cc65c6f78b0253d995adea4ae59e
RakeshThapa332 <thapaarakesh1@gmail.com>
2026-08-15 15:24:43 +0545

    Authentication page handles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,18 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 
 import App from "./App";
 import { theme } from "./theme/theme";
+import { AuthProvider } from "./context/AuthContext";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+      
     </ThemeProvider>
   </React.StrictMode>
 );
